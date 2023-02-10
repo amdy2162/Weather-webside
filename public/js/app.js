0f594898e13b9ba4eb49b022f68222ee54ba5467
andy <amdy2162@hotmail.com>
2023-02-10 14:08:21 +0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherFrom.addEventListener('submit', (e) => {
     MessageOne.textContent = 'Loading...';
     MessageTwo.textContent= '';
     const location = search.value;
-    fetch('http://localhost:3000/weather?address='+ location ).then((res) => {
+    fetch('/weather?address='+ location ).then((res) => {
     res.json().then((data) => {
         if(data.error) {
             // console.log(data.error);
